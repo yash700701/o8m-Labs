f2ac76d7a7a6c815fa4dad3c8eb740ca3df290f5
yash tiwari <yashtiwari700714@gmail.com>
2026-02-10 21:21:26 +0530

    change port

--- backend/src/server.js
+++ backend/src/server.js
@@ -7,7 +7,7 @@ dotenv.config({ path: path.resolve(__dirname, "..", ".env") });
 
 import app from "./app.js";
 
-const PORT = process.env.PORT || 5000;
+const PORT = 8000;
 
 app.listen(PORT, () => {
     console.log(`Backend running on port ${PORT}`);
